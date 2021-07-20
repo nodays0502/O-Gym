9375d13658a26b59afd45c9ac5b65a70db7479e8
SeYeonCho <cnsy831@gmail.com>
2021-07-20 22:42:13 +0900

    [S05P12B305-19] init: PTTeacher Entity 구조 설계 및 매핑

--- BackEnd/ogym/src/main/java/com/B305/ogym/domain/users/ptTeacher/PTTeacher.java
+++ BackEnd/ogym/src/main/java/com/B305/ogym/domain/users/ptTeacher/PTTeacher.java
@@ -1,4 +1,40 @@
 package com.B305.ogym.domain.users.ptTeacher;
 
-public class PTTeacher {
+import com.B305.ogym.domain.users.common.UserBase;
+import com.B305.ogym.domain.users.ptStudent.PtStudent;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class PTTeacher extends UserBase {
+    // 평점
+    private Integer starRating;
+
+    // 전문분야
+    private String major;
+
+    // 경력 리스트
+    @OneToMany(mappedBy = "id")
+    private List<Certificate> certificates = new ArrayList<>();
+
+    // 가격
+    private Integer price;
+
+    // 자기소개
+    private String description;
+
+    // SNS 링크
+    private String snsAddr;
+
+    // 학생 리스트
+    @OneToMany(mappedBy = "")
+    private Set<PtStudent> students = new LinkedHashSet<>();
+
+    // 이력?
 }
