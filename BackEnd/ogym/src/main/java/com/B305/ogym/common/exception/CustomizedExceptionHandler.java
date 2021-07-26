1dd7d34b435f86cd9fb975e6d7b6a29422479bad
SeYeonCho <cnsy831@gmail.com>
2021-07-26 15:08:23 +0900

    feat : 5xx 에러에 대한 AllExceptionsHandler 적용

--- BackEnd/ogym/src/main/java/com/B305/ogym/common/exception/CustomizedExceptionHandler.java
+++ BackEnd/ogym/src/main/java/com/B305/ogym/common/exception/CustomizedExceptionHandler.java
@@ -1,2 +1,26 @@
-package com.B305.ogym.common.exception;public class CustomizedExceptionHandler {
+package com.B305.ogym.common.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestController
+@ControllerAdvice
+public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // 5xx error handler : 서버에서 발생한 에러에 대한 핸들러
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> AllExceptionsHandler(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                                    .Error(request.getDescription(false))
+                                    .message(ex.getMessage())
+                                    .build();
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
--- BackEnd/ogym/src/main/java/com/B305/ogym/common/exception/ExceptionResponse.java
+++ BackEnd/ogym/src/main/java/com/B305/ogym/common/exception/ExceptionResponse.java
@@ -1,2 +1,21 @@
-package com.B305.ogym.common.exception;public class ExceptionResponse {
+package com.B305.ogym.common.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ExceptionResponse {
+
+    private String Error;
+    private String message;
+
+    @Builder
+    ExceptionResponse(String Error, String message) {
+        this.Error = Error;
+        this.message = message;
+    }
 }
