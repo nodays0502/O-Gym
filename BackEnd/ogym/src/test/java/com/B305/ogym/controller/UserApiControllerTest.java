270cef40ec335688431e2ea2affb2f08b91ec7da
KJH-0105 <juhyun7955@gmail.com>
2021-07-29 17:15:22 +0900

    ﻿[S05P12B305-57] fix : Spring rest docs html이 생기지 않는 버그 해결

--- BackEnd/ogym/src/test/java/com/B305/ogym/controller/UserApiControllerTest.java
+++ BackEnd/ogym/src/test/java/com/B305/ogym/controller/UserApiControllerTest.java
@@ -1,11 +1,21 @@
 package com.B305.ogym.controller;
 
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.SharedHttpSessionConfigurer.sharedHttpSession;
 
+import com.B305.ogym.common.config.SecurityConfig;
 import com.B305.ogym.controller.dto.UserDto.SignupRequest;
 import com.B305.ogym.domain.autority.Authority;
 import com.B305.ogym.domain.autority.AuthorityRepository;
 import com.B305.ogym.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,20 +26,29 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @ExtendWith(RestDocumentationExtension.class) // JUnit 5 사용시 문서 스니펫 생성용
-@WebMvcTest(UserApiController.class) // Controller 관련 빈들만 등록됨
+@WebMvcTest(controllers = UserApiController.class, excludeFilters = {
+    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)}
+)
 //@ActiveProfiles("test") // 테스트에서 사용할 profile
 @MockBean(JpaMetamodelMappingContext.class) // @EnableJPaAuditing 사용시 추가해야하는 어노테이션
-@AutoConfigureRestDocs
+//@AutoConfigureRestDocs
+@AutoConfigureMockMvc(addFilters = false)
 class UserApiControllerTest {
 
     @MockBean
@@ -38,6 +57,19 @@ class UserApiControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup(WebApplicationContext webApplicationContext,
+        RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply(documentationConfiguration(restDocumentation))
+            .apply(sharedHttpSession())
+            .addFilters(new CharacterEncodingFilter("UTF-8", true))
+            .build();
+    }
+
     @DisplayName("회원가입 - 모든 유효성 검사에 통과했다면, 회원가입 성공")
     @Test
     public void createUser_success() throws Exception {
@@ -45,20 +77,48 @@ class UserApiControllerTest {
         System.out.println(1);
         SignupRequest signupRequest = SignupRequest.builder()
             .email("hello@naver.com")
-            .password("1234")
-            .username("주현")
-            .nickname("펭귄")
+            .password("asdasd")
+            .username("juhu")
+            .nickname("juhu")
             .gender(0)
             .tel("010-0000-0000")
             .zipCode("12345")
-            .street("대농로 17")
-            .detailedAddress("상세주소상세주소")
-            .role("ROLE_PTSTUDENT")
+            .street("road 17")
+            .detailedAddress("juhu")
+            .role("ROLE_PTTEACHER")
             .build();
         //when
         doNothing().when(userService).signup(signupRequest);
 
         //then
+        mockMvc.perform(post("/api/signup")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(signupRequest)))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("userApi/signup/successful", requestFields(
+                fieldWithPath("email").type(JsonFieldType.STRING)
+                    .description("The user's email address"),
+                fieldWithPath("password").type(JsonFieldType.STRING)
+                    .description("The user's password"),
+                fieldWithPath("username").type(JsonFieldType.STRING)
+                    .description("The user's username"),
+                fieldWithPath("nickname").type(JsonFieldType.STRING)
+                    .description("The user's nickname"),
+                fieldWithPath("gender").type(JsonFieldType.NUMBER)
+                    .description("The user's gender"),
+                fieldWithPath("tel").type(JsonFieldType.STRING)
+                    .description("The user's tel"),
+                fieldWithPath("zipCode").type(JsonFieldType.STRING)
+                    .description("The user's zipCode"),
+                fieldWithPath("street").type(JsonFieldType.STRING)
+                    .description("The user's street"),
+                fieldWithPath("detailedAddress").type(JsonFieldType.STRING)
+                    .description("The user's detailedAddress"),
+                fieldWithPath("role").type(JsonFieldType.STRING)
+                    .description("The user's role")
+            )));
+
     }
 
     @DisplayName("회원가입 - 이메일 중복으로 인한 회원가입 실패")
