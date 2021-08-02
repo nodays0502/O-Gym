4cce59d845b562d72a218b28263d340627bb7f20
KJH-0105 <juhyun7955@gmail.com>
2021-08-02 23:03:24 +0900

    bugfix : cors test

--- BackEnd/src/main/java/com/example/demo/testController.java
+++ BackEnd/src/main/java/com/example/demo/testController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://ec2-13-124-95-248.ap-northeast-2.compute.amazonaws.com:32286")
+
 @RestController
 public class testController {
 
+    @CrossOrigin(origins="*")
     @GetMapping(value = "/")
     public String hello() {
         return "Hello World!!";
