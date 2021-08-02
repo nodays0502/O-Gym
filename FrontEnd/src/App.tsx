bba7e8661db2b646fffa5b255ece62b7e1d47115
IMHOJEONG <39ghwjd@naver.com>
2021-08-03 01:44:34 +0900

    chore: k8s 내부 주소  변경 후 빌드

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   
   useEffect(() => {
     async function fetchData() {
-      const result = await axios.get(`http://10.152.183.31:8081`);
+      const result = await axios.get(`http://ec2-13-124-95-248.ap-northeast-2.compute.amazonaws.com:32286/`);
       setData(result.data);
     }
     fetchData();
