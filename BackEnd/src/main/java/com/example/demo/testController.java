f12a4b3243b6a3fff05cf66a5dd0211e0b625340
KJH-0105 <juhyun7955@gmail.com>
2021-08-02 23:14:22 +0900

    bugfix : cors fix

--- BackEnd/src/main/java/com/example/demo/testController.java
+++ BackEnd/src/main/java/com/example/demo/testController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,8 @@ public class testController {
 
     @CrossOrigin("*")
     @GetMapping(value = "/")
-    public String hello() {
+    public String hello(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return "Hello World!!";
     }
 }
