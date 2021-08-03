1dff105f29b6fc707f8fc364e077a7a6c0113678
nodays0502 <nodayst@naver.com>
2021-08-03 10:28:31 +0900

    [S05P13B305-24] feature : 학생 리스트 건강 조회 API

--- BackEnd/ogym/src/main/java/com/B305/ogym/domain/users/ptStudent/PTStudentRepository.java
+++ BackEnd/ogym/src/main/java/com/B305/ogym/domain/users/ptStudent/PTStudentRepository.java
@@ -3,5 +3,5 @@ package com.B305.ogym.domain.users.ptStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PTStudentRepository extends JpaRepository<PTStudent,Long> {
-
+    PTStudent findByEmail(String email);
 }
