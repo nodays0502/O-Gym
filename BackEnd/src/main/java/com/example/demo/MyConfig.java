db940719ea9a94a5c5109c1f63c0f27950e8f1f9
KJH-0105 <juhyun7955@gmail.com>
2021-08-03 15:48:31 +0900

    bugfix : CORS test

--- BackEnd/src/main/java/com/example/demo/MyConfig.java
+++ BackEnd/src/main/java/com/example/demo/MyConfig.java
@@ -3,14 +3,21 @@ package com.example.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
-public class MyConfig implements WebMvcConfigurer{
+@EnableWebMvc
+public class MyConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS")
+            .allowCredentials(true)
+        ;
     }
 }
--- BackEnd/src/main/java/com/example/demo/testController.java
+++ BackEnd/src/main/java/com/example/demo/testController.java
@@ -4,15 +4,24 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
 public class testController {
 
     @CrossOrigin("*")
-    @GetMapping(value = "/")
+    @GetMapping(value = "/hello")
     public String hello(HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "true");
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return "Hello World!!";
     }
+
+//    @CrossOrigin("*")
+//    @GetMapping(value = "/")
+//    public String proxyHello() {
+//        String url = "http://ec2-13-124-95-248.ap-northeast-2.compute.amazonaws.com:30044/hello";
+//        RestTemplate restTemplate = new RestTemplate();
+//        return restTemplate.getForObject(url, String.class);
+//    }
 }
