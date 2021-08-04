65cbccf5337f965f77228b35180938e270c10b6e
KJH-0105 <juhyun7955@gmail.com>
2021-08-04 18:08:53 +0900

    ﻿test : student 회원가입 test 작성

--- BackEnd/src/main/java/com/B305/ogym/controller/dto/UserDto.java
+++ BackEnd/src/main/java/com/B305/ogym/controller/dto/UserDto.java
@@ -6,7 +6,6 @@ import com.B305.ogym.domain.users.ptStudent.PTStudent;
 import com.B305.ogym.domain.users.ptTeacher.Career;
 import com.B305.ogym.domain.users.ptTeacher.Certificate;
 import com.B305.ogym.domain.users.ptTeacher.PTTeacher;
-import com.B305.ogym.domain.users.ptTeacher.Sns;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +22,181 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class UserDto {
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SaveTeacherRequest {
+
+        @NotEmpty
+        @Size(min = 3, max = 50, message = "이메일 똑바로 입력하세요.")
+        @Pattern(regexp = "\\S+@\\S+(.com)$")
+        private String email;
+
+        //    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+        @NotEmpty
+        @Size(min = 3, max = 20, message = "비밀번호는 3자이상 20자 이하로 입력해주세요.")
+        private String password;
+
+        @NotEmpty
+        @Size(min = 2, max = 10, message = "올바르게 이름을 입력하세요.")
+        private String username;
+
+        @NotEmpty
+        @Size(min = 3, max = 50, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
+        private String nickname;
+
+        @NotNull(message = "성별 입력하세요.")
+        private Integer gender; // 0 MAN , 1 WOMAN
+
+        @NotEmpty(message = "올바른 휴대폰 번호를 입력하세요.")
+        @Pattern(regexp = "\\d{3}-\\d{3,4}-\\d{4}")
+        private String tel;
+
+        @NotEmpty
+        @Pattern(regexp = "\\d{5}", message = "올바른 우편번호를 입력하세요.")
+        private String zipCode; // 우편번호
+
+        @NotEmpty(message = "도로명 주소를 입력하세요.")
+        private String street; // 도로명 주소
+
+        @NotEmpty(message = "상세주소를 입력하세요.")
+        private String detailedAddress; // 상세 주소 ?
+
+        @NotEmpty(message = "권한 주세요.")
+        private String role;
+
+        @NotBlank
+        private String major;
+
+        //        @NotEmpty
+        private List<Certificate> certificates;
+
+        //        @NotEmpty
+        public List<Career> careers;
+
+        @NotNull
+        private int price;
+
+        @NotBlank
+        private String description;
+
+        //        @NotEmpty
+        private List<SnsDto> snsAddrs;
+
+        @Getter
+        @NoArgsConstructor(access = AccessLevel.PUBLIC)
+        static class SnsDto {
+
+            private String snsUrl;
+            private String snsName;
+        }
+
+        public PTTeacher toEntity() {
+
+            Gender gender = Gender.MAN;
+            if (this.gender == 1) {
+                gender = Gender.WOMAN;
+            }
+
+            Address address = Address.createAddress(
+                zipCode,
+                street,
+                detailedAddress
+            );
+
+            return PTTeacher.builder()
+                .email(email)
+                .username(username)
+                .nickname(nickname)
+                .gender(gender)
+                .tel(tel)
+                .address(address)
+                .description(description)
+                .price(price)
+                .major(major)
+                .certificates(new ArrayList<>())
+                .careers(new ArrayList<>())
+                .build();
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SaveStudentRequest {
+
+        @NotEmpty
+        @Size(min = 3, max = 50, message = "이메일 똑바로 입력하세요.")
+        @Pattern(regexp = "\\S+@\\S+(.com)$")
+        private String email;
+
+        //    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+        @NotEmpty
+        @Size(min = 3, max = 20, message = "비밀번호는 3자이상 20자 이하로 입력해주세요.")
+        private String password;
+
+        @NotEmpty
+        @Size(min = 2, max = 10, message = "올바르게 이름을 입력하세요.")
+        private String username;
+
+        @NotEmpty
+        @Size(min = 3, max = 50, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
+        private String nickname;
+
+        @NotNull(message = "성별 입력하세요.")
+        private Integer gender; // 0 MAN , 1 WOMAN
+
+        @NotEmpty(message = "올바른 휴대폰 번호를 입력하세요.")
+        @Pattern(regexp = "\\d{3}-\\d{3,4}-\\d{4}")
+        private String tel;
+
+        @NotEmpty
+        @Pattern(regexp = "\\d{5}", message = "올바른 우편번호를 입력하세요.")
+        private String zipCode; // 우편번호
+
+        @NotEmpty(message = "도로명 주소를 입력하세요.")
+        private String street; // 도로명 주소
+
+        @NotEmpty(message = "상세주소를 입력하세요.")
+        private String detailedAddress; // 상세 주소 ?
+
+        @NotEmpty(message = "권한 주세요.")
+        private String role;
+
+        @NotEmpty
+        private List<Integer> monthlyHeights;
+
+        @NotEmpty
+        private List<Integer> monthlyWeights;
+
+        public PTStudent toEntity() {
+
+            Gender gender = Gender.MAN;
+            if (this.gender == 1) {
+                gender = Gender.WOMAN;
+            }
+
+            Address address = Address.createAddress(
+                zipCode,
+                street,
+                detailedAddress
+            );
+
+            return PTStudent.builder()
+                .email(email)
+                .username(username)
+                .nickname(nickname)
+                .gender(gender)
+                .tel(tel)
+                .address(address)
+                .build();
+        }
+
+    }
+
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -65,83 +239,6 @@ public class UserDto {
 
         @NotEmpty(message = "권한 주세요.")
         private String role;
-
-        // --------------------------
-//        @NotEmpty
-        private List<Integer> monthlyHeights;
-
-//        @NotEmpty
-        private List<Integer> monthlyWeights;
-
-        public PTStudent toPTStudentEntity() {
-
-            Gender gender = Gender.MAN;
-            if (this.gender == 1) {
-                gender = Gender.WOMAN;
-            }
-
-            Address address = Address.createAddress(
-                zipCode,
-                street,
-                detailedAddress
-            );
-
-            return PTStudent.builder()
-                .email(email)
-                .username(username)
-                .nickname(nickname)
-                .gender(gender)
-                .tel(tel)
-                .address(address)
-                .build();
-        }
-        // ----------
-
-//        @NotBlank
-        private String major;
-
-        //        @NotEmpty
-        private List<Certificate> certificates;
-
-        //        @NotEmpty
-        public List<Career> careers;
-
-//        @NotNull
-        private int price;
-
-//        @NotBlank
-        private String description;
-
-        //        @NotEmpty
-        private List<Sns> snsAddrs;
-
-        public PTTeacher toPTTeacherEntity() {
-
-            Gender gender = Gender.MAN;
-            if (this.gender == 1) {
-                gender = Gender.WOMAN;
-            }
-
-            Address address = Address.createAddress(
-                zipCode,
-                street,
-                detailedAddress
-            );
-
-            return PTTeacher.builder()
-                .email(email)
-                .username(username)
-                .nickname(nickname)
-                .gender(gender)
-                .tel(tel)
-                .address(address)
-                .description(description)
-                .price(price)
-                .major(major)
-                .certificates(new ArrayList<>())
-                .careers(new ArrayList<>())
-                .build();
-        }
     }
 
 
@@ -155,7 +252,8 @@ public class UserDto {
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PUBLIC)
-    public static class CareerDto{
+    public static class CareerDto {
+
         private String role;
 
         private LocalDate startDate;
@@ -166,7 +264,7 @@ public class UserDto {
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PUBLIC)
-    public static class CertificateDto{
+    public static class CertificateDto {
 
         private String name; // 자격증 명칭
 
