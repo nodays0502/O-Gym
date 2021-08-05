7d441818c6555533ced187e6ddd40a15d695caa1
IMHOJEONG <39ghwjd@naver.com>
2021-08-06 02:30:10 +0900

    chore: nginx reverse proxy port change 8888->8887

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   
   useEffect(() => {
     async function fetchData() {
-      const result = await axios.get(`http://13.124.95.248:8888`);
+      const result = await axios.get(`http://13.124.95.248:8887`);
       console.log(result);
       setData(result.data);
     }
