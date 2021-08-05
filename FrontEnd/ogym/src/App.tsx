0ca6cff75fe43a737ee84c3373c0b076e9505fae
IMHOJEONG <39ghwjd@naver.com>
2021-08-06 00:03:48 +0900

    chore : add dockerfile and nginx.conf

--- FrontEnd/ogym/src/App.tsx
+++ FrontEnd/ogym/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import MainPage from './pages/MainPage/MainPage';
+import SessionPage from './pages/WebRtcPage/SessionPage';
 
 function App() {
   return (
     <>
       <MainPage></MainPage>
+      {/* <SessionPage/> */}
     </>
   );
 }
