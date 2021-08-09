2e2609d298d63bf7e4cdba6ad353ffbc8f73e6df
SeYeonCho <cnsy831@gmail.com>
2021-08-10 01:08:22 +0900

    refactor: 예약정보 중복 Exception 추가 및 api 리팩토링

--- BackEnd/src/main/java/com/B305/ogym/controller/dto/HealthDto.java
+++ BackEnd/src/main/java/com/B305/ogym/controller/dto/HealthDto.java
@@ -3,14 +3,22 @@ package com.B305.ogym.controller.dto;
 import com.B305.ogym.domain.users.common.Gender;
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 public class HealthDto {
 
+    // 건강정보 조회시 사용
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class MyHealthResponse {
@@ -22,6 +30,28 @@ public class HealthDto {
         public MyHealthResponse(List<Integer> heightList, List<Integer> weightList) {
             this.heightList = heightList;
             this.weightList = weightList;
+        }
+    }
+
+    // 건강정보 수정시 사용
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SetMyHealthRequest {
+        @Min(1)
+        @Max(12)
+        @NotNull
+        private int month;
+
+        @Positive
+        private int height;
+        @Positive
+        private int weight;
+
+        @Builder
+        public SetMyHealthRequest(int month, int height, int weight){
+            this.month = month;
+            this.height = height;
+            this.weight = weight;
         }
     }
 
--- BackEnd/src/main/java/com/B305/ogym/service/HealthService.java
+++ BackEnd/src/main/java/com/B305/ogym/service/HealthService.java
@@ -2,13 +2,18 @@ package com.B305.ogym.service;
 
 import com.B305.ogym.controller.dto.HealthDto;
 import com.B305.ogym.controller.dto.HealthDto.MyHealthResponse;
+import com.B305.ogym.controller.dto.HealthDto.SetMyHealthRequest;
 import com.B305.ogym.domain.users.UserRepository;
 import com.B305.ogym.domain.users.common.UserBase;
+import com.B305.ogym.domain.users.ptStudent.Monthly;
+import com.B305.ogym.domain.users.ptStudent.MonthlyRepository;
 import com.B305.ogym.domain.users.ptStudent.PTStudent;
 import com.B305.ogym.domain.users.ptStudent.PTStudentRepository;
 import com.B305.ogym.domain.users.ptTeacher.PTTeacherRepository;
+import com.B305.ogym.exception.health.HealthDuplicateException;
 import com.B305.ogym.exception.user.UnauthorizedException;
 import com.B305.ogym.exception.user.UserNotFoundException;
+import com.sun.jdi.request.DuplicateRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,11 +25,12 @@ public class HealthService {
 
     private final PTTeacherRepository ptTeacherRepository;
     private final PTStudentRepository ptStudentRepository;
+    private final MonthlyRepository monthlyRepository;
     private final UserRepository userRepository;
 
     // 로그인한 사용자의 건강정보 조회
     @Transactional
-    public MyHealthResponse getMyHealthResponse(String userEmail) {
+    public MyHealthResponse getMyHealth(String userEmail) {
 //        UserBase user = userRepository.findByEmail(userEmail)
 //            .orElseThrow(() -> new UserNotFoundException("해당하는 이메일이 존재하지 않습니다."));
 //        if (!user.getRole().equals("ROLE_PTSTUDENT")) {
