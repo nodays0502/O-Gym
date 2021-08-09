b06d6b661aa2f6bb13ebe00360974056bf3b3457
SeYeonCho <cnsy831@gmail.com>
2021-08-09 20:51:22 +0900

    refactor: reservation param(id -> teacher_email, reservation_Time), [cancle / make] reservation DTO 통합

--- BackEnd/src/main/java/com/B305/ogym/controller/PTApiController.java
+++ BackEnd/src/main/java/com/B305/ogym/controller/PTApiController.java
@@ -2,20 +2,16 @@ package com.B305.ogym.controller;
 
 import com.B305.ogym.controller.dto.PTDto;
 import com.B305.ogym.controller.dto.SuccessResponseDto;
-import com.B305.ogym.controller.dto.UserDto;
 import com.B305.ogym.domain.users.common.UserBase;
 import com.B305.ogym.service.PTService;
 import java.util.HashMap;
 import java.util.Map;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +28,7 @@ public class PTApiController {
     @PreAuthorize("hasAnyRole('PTSTUDENT')")
     public ResponseEntity<SuccessResponseDto> makeReservation(
         @AuthenticationPrincipal UserBase user,
-        @RequestBody @Valid PTDto.SaveReservationRequest request){
+        @RequestBody @Valid PTDto.reservationRequest request){
 
         System.out.println("Controller - useremail:" + user.getEmail());
 
@@ -47,10 +43,10 @@ public class PTApiController {
     @PreAuthorize("hasAnyRole('PTSTUDENT')")
     public ResponseEntity<SuccessResponseDto> cancleReservation(
         @AuthenticationPrincipal UserBase user,
-        @RequestBody @Valid PTDto.CancelReservationRequest request
+        @RequestBody @Valid PTDto.reservationRequest request
     ){
         System.out.println("controller" + user.getRole());
-        ptService.cancleReservation(user, request);
+        ptService.cancleReservation(user.getEmail(), request);
         return ResponseEntity.ok(new SuccessResponseDto<Map>(
             200, "PT 삭제에 성공했습니다.", new HashMap()
         ));
