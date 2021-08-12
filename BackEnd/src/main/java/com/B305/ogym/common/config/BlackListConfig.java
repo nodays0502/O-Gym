a3b182c1da8e3870b3fd7050ce3fc6de83c3fbb7
KJH-0105 <juhyun7955@gmail.com>
2021-08-12 20:30:29 +0900

    feat : Redis 분할해서 refreshToken 서버, blacklist 기법 적용한 탈퇴한 회원용 AccessToken 서버 분리, 관련하여 로그인, 회원탈퇴 관련 로직 수정

---
+++ BackEnd/src/main/java/com/B305/ogym/common/config/BlackListConfig.java
@@ -0,0 +1,34 @@
+package com.B305.ogym.common.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class BlackListConfig {
+
+    @Value("${spring.redis.blacklist.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.blacklist.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisBlackListConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean(name = "redisBlackListTemplate")
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisBlackListConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+}
--- BackEnd/src/main/java/com/B305/ogym/domain/authority/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.B305.ogym.domain.authority;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken,String> {
-
-}
