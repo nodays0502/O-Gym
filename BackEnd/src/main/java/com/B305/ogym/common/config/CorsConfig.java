e7c383e3bafac1d4b8223d6a862109f9f3990d9d
nodays0502 <nodayst@naver.com>
2021-08-12 20:14:32 +0900

    bugfix : cors 수정

--- BackEnd/src/main/java/com/B305/ogym/common/config/CorsConfig.java
+++ BackEnd/src/main/java/com/B305/ogym/common/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
+//        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
