a9b54d2d3481d70818c76fc2fa22f8fa17f45742
KJH-0105 <juhyun7955@gmail.com>
2021-08-13 17:15:37 +0900

    test : testController 삭제, HealthService Controller 추가

--- BackEnd/src/test/java/com/B305/ogym/service/HealthServiceTest.java
+++ BackEnd/src/test/java/com/B305/ogym/service/HealthServiceTest.java
@@ -2,6 +2,30 @@ package com.B305.ogym.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.B305.ogym.domain.users.UserRepository;
+import com.B305.ogym.domain.users.ptStudent.MonthlyRepository;
+import com.B305.ogym.domain.users.ptStudent.PTStudentRepository;
+import com.B305.ogym.domain.users.ptTeacher.PTTeacherRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class HealthServiceTest {
+
+
+    @Mock
+    PTTeacherRepository ptTeacherRepository;
+    @Mock
+    PTStudentRepository ptStudentRepository;
+    @Mock
+    MonthlyRepository monthlyRepository;
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    HealthService healthService;
+
 
 }
